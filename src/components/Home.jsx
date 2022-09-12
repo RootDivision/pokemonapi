8f9ebe60a8fb31ac5d00f6910a7b8dbd1d3bf02e
Vuong Ha <vuong.ha@politie.antwerpen.be>
2022-09-12 18:54:42 +0200

    Add project files

---
+++ src/components/Home.jsx
@@ -0,0 +1,55 @@
+import { useEffect, useState } from "react"
+
+const Home = () => {
+  const [pokemons, setPokemons] = useState()
+  const [url, setUrl] = useState('https://pokeapi.co/api/v2/pokemon')
+
+  const fetchPokemon = async () => {
+    const response = await fetch(url)
+    const json = await response.json()
+    setPokemons(json)
+  }
+
+  useEffect(() => {
+    fetchPokemon()
+  }, [url])
+
+  const nextPage = () => {
+    setUrl(pokemons.next)
+  }
+  
+  const prevPage = () => {
+    setUrl(pokemons.next)
+  }
+
+  return (
+    <>
+      <h1>Pokemon</h1>
+      {
+        pokemons ? (
+          <ul>
+            {
+              pokemons.results.map((pokemon) => {
+                const { name } = pokemon
+                return (
+                  <li key={name}>{name}</li>
+                )
+              })
+            }
+          </ul>
+        ) : (
+          <p>
+            bezig met laden
+          </p>
+        )
+      }
+
+      <div>
+        <button onClick={() => prevPage()}>prev</button>
+        <button onClick={() => nextPage()}>next</button>
+      </div>
+    </>
+  )
+}
+
+export default Home
